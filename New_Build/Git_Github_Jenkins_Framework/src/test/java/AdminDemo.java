30d52d85f4b253206c549de35f260f7a40a9be8f
bhuiyan30 <abujabed30@gmail.com>
2022-12-17 06:48:37 -0500

    commit initial

---
+++ New_Build/Git_Github_Jenkins_Framework/src/test/java/AdminDemo.java
@@ -0,0 +1,14 @@
+public class AdminDemo {
+
+
+    public static void main(String[] args) {
+        //AdminDemo adminDemo =new AdminDemo();
+  // scanner();
+    }
+    static void scanner(){
+        System.out.println("Brooklyn city");
+
+
+    }
+
+}
---
+++ New_Build/Git_Github_Jenkins_Framework/src/test/java/UsersDemo.java
@@ -0,0 +1,21 @@
+
+
+
+public class UsersDemo {
+
+
+    public static void main(String[] args) {
+      //  UsersDemo obj=new UsersDemo();
+       // loopsdemo();
+    }
+
+    static void loopsdemo(){
+        int []num={1,8,9,4,9};
+        for (int i=0;i<num.length; i++){
+            System.out.println(i);
+        }
+    }
+
+
+
+}
